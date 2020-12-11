ddcd759e30b3eb37a64b73170bdff8e6589e303c
ilovetocode2019 <54194229+ilovetocode2019@users.noreply.github.com>
2020-12-11 15:32:05 -0800

    Fix typo

--- limits/__init__.py
+++ limits/__init__.py
@@ -3,4 +3,4 @@ import collections
 __version__ = "0.1.0a"
 
 VersionInfo = collections.namedtuple("VersionInfo", "major minor micro releaselevel serial")
-version_info = VersionInfo(major=0, minor=3, micro=1, releaselevel="final", serial=0)
+version_info = VersionInfo(major=0, minor=1, micro=1, releaselevel="final", serial=0)
